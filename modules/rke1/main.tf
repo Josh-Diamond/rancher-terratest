895a38ed4e149a4394fa1c30eca76ae513736316
Josh Diamond <joshdiamondthedev@gmail.com>
2022-05-19 18:31:33 -0700

    getProvisioningClusterID, waitUntilRke2ClustersActive

--- modules/rke1/main.tf
+++ modules/rke1/main.tf
@@ -17,6 +17,7 @@ resource "rancher2_cloud_credential" "rancher2_cloud_credential" {
   amazonec2_credential_config {
     access_key = var.aws_access_key
     secret_key = var.aws_secret_key
+    default_region = var.aws_region
   }
 }
 
