8c1a13d2b3646b54de8d270d7c64911a09c55030
Josh Diamond <joshdiamondthedev@gmail.com>
2022-05-24 13:48:34 -0700

    GetUserID() + refactored GetRancherServerVersion() to grab version from local cluster, not downstream

--- functions/getRancherServerVersion.go
+++ functions/getRancherServerVersion.go
@@ -4,20 +4,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func GetRancherServerVersion(hostURL string, clusterID string, token string) string {
+// From local cluster
+func GetRancherServerVersion(hostURL string, token string) string {
+	type nestedData struct {
+		Version string `json:"seen-whatsnew"`
+	}
+
 	type clusterSpecs struct {
-		Name  string `json:"name"`
-		Value string `json:"value"`
+		Id  string `json:"id"`
+		Dataa nestedData `json:"data"`
 	}
+
 	type clusterResponse struct {
-		AgentEnvVars []clusterSpecs `json:"appliedAgentEnvVars"`
+		Data []clusterSpecs `json:"data"`
 	}
 
-	url := fmt.Sprintf("%s/v3/clusters/%s", hostURL, clusterID)
+	url := fmt.Sprintf("%s/v1/userpreferences", hostURL)
 
-	var agentEnvVars clusterResponse
+	var response clusterResponse
 
 	client := http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
@@ -36,18 +43,67 @@ func GetRancherServerVersion(hostURL string, clusterID string, token string) str
 
 	defer res.Body.Close()
 
-	jsonErr := json.NewDecoder(res.Body).Decode(&agentEnvVars)
+	jsonErr := json.NewDecoder(res.Body).Decode(&response)
 	if err != nil {
 		fmt.Printf("%v", jsonErr)
 	}
 
-	var serverVersion string
+	clippedID := strings.Trim(response.Data[0].Dataa.Version, `"`)
 
-	for _, vars := range agentEnvVars.AgentEnvVars {
-		if vars.Name == "CATTLE_SERVER_VERSION" {
-			serverVersion = vars.Value
-		}
-	}
-
-	return serverVersion
+	return clippedID
 }
+
+
+
+
+// local seems to be more reliable as it is available as soon as the server is launched; 
+// Downstream needs cluster before it can retrieved
+//
+//
+// From downstream cluster
+//
+// func GetRancherServerVersion(hostURL string, clusterID string, token string) string {
+// 	type clusterSpecs struct {
+// 		Name  string `json:"name"`
+// 		Value string `json:"value"`
+// 	}
+// 	type clusterResponse struct {
+// 		AgentEnvVars []clusterSpecs `json:"appliedAgentEnvVars"`
+// 	}
+
+// 	url := fmt.Sprintf("%s/v3/clusters/%s", hostURL, clusterID)
+
+// 	var agentEnvVars clusterResponse
+
+// 	client := http.Client{}
+// 	req, err := http.NewRequest("GET", url, nil)
+// 	if err != nil {
+// 		fmt.Printf("%v", err)
+// 	}
+
+// 	req.Header = http.Header{
+// 		"Authorization": []string{token},
+// 	}
+
+// 	res, clientErr := client.Do(req)
+// 	if clientErr != nil {
+// 		fmt.Printf("%v", clientErr)
+// 	}
+
+// 	defer res.Body.Close()
+
+// 	jsonErr := json.NewDecoder(res.Body).Decode(&agentEnvVars)
+// 	if err != nil {
+// 		fmt.Printf("%v", jsonErr)
+// 	}
+
+// 	var serverVersion string
+
+// 	for _, vars := range agentEnvVars.AgentEnvVars {
+// 		if vars.Name == "CATTLE_SERVER_VERSION" {
+// 			serverVersion = vars.Value
+// 		}
+// 	}
+
+// 	return serverVersion
+// }
